5698e2bab0731c711f5d3b0117cd275d67878c97
Rian Lucas <rianlucas2014@gmail.com>
2024-06-04 17:32:01 -0300

    feat: authentication

---
+++ src/http/routes/auth/sign-up.ts
@@ -0,0 +1,36 @@
+import z from 'zod'
+import bcrypt from 'bcrypt'
+import { prisma } from '../../../lib/prisma'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import jwt from 'jsonwebtoken'
+
+export async function handleSignUp(req: FastifyRequest, res: FastifyReply) {
+  const signUpBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    phoneNumber: z.string()
+  })
+
+  const { name, email, password, phoneNumber } = signUpBody.parse(
+    req.body,
+  )
+
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  const created = await prisma.user.create({
+    data: {
+      email,
+      name,
+      phoneNumber,
+      password: hashedPassword,
+    },
+  })
+
+  const token = jwt.sign(created.id, process.env.JWT_PASS ?? '')
+
+  return res.status(201).send({
+    id: created.id,
+    token,
+  })
+}
