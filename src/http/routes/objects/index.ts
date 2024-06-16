078bc5cc5c09a33ed14b6cae765f45e3ae09c509
Rian Lucas <rianlucas2014@gmail.com>
2024-06-16 14:43:00 -0300

    feat: enable auth

--- src/http/routes/objects/index.ts
+++ src/http/routes/objects/index.ts
@@ -6,7 +6,7 @@ import { handleDevolution } from './devolution'
 
 
 export async function objectsHandler(app: FastifyInstance) {
-  // app.addHook('preHandler', authMiddleware)
+  app.addHook('preHandler', authMiddleware)
   app.post('/', handleCreateObject)
   app.get('/', handleFindObjectsByCategory)
   app.post('/devolution', handleDevolution)
